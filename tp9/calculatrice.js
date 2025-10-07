60b1cc6df0ef22327ab2de376abb1046ecd4e2a9
oznur77 <oznursari077@gmail.com>
2025-10-07 15:12:31 +0200

    Add files via upload

---
+++ tp9/calculatrice.js
@@ -0,0 +1,8 @@
+function somme()
+{
+
+var a = document.getElementById("t1").ariaValueMax;
+var b = document.getElementById("t2").ariaValueMax;
+var c = Number(a) + Number(b);
+document.getElementById("resultat").value = c;
+}
